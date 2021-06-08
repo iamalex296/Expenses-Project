adffc13c465fa798179fbfbe8d4f4d7f4dcded58
alex <alexizana10@gmail.com>
2021-06-08 15:42:03 +0400

    added new expense components with inputs for adding new expenses

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const { date, title, amount } = props;
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("updated!");
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
